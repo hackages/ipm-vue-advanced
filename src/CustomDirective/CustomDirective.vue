5e5886016ada54ad5a1e5c8c2514c8f348c8746b
Boubacar S. BARRY <bouba@hackages.io>
2019-01-23 11:52:17 +0100

    Init

---
+++ src/CustomDirective/CustomDirective.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p v-hc-hidden="true">Hide me</p>
+        <p v-hc-if="false">Custom:Hide me</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Model',
+  data() {
+    return {
+      datePicker: {},
+      check: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/CustomDirective/HcIfDirective.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+Vue.directive('hc-if', function(el, binding, vNode) {
+});
+
+function commentNode(el, vnode) {
+}
